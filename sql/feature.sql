b70fea3f4f85a70dcb845e0fecd84619a7f30da5
tamtadam <trenyik.adam@gmail.com>
2016-12-04 22:27:57 +0100

    Fix SQL syntax error

--- sql/feature.sql
+++ sql/feature.sql
@@ -2,5 +2,5 @@ CREATE TABLE Feature (
   FeatureID  int(10)      unsigned         NOT NULL AUTO_INCREMENT,
   Title      varchar(100)                  NOT NULL,
   Locked     tinyint(1)   unsigned DEFAULT 0,
-  PRIMARY KEY (FeatureID),
+  PRIMARY KEY (FeatureID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
